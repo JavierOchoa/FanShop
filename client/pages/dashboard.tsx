8f1323ad3e6385a664b517c815498878fc1aedeb
Javier Ochoa <jochoaalv@gmail.com>
2023-02-17 22:31:37 -0500

    Dashboard is inaccessible if there is no active user.

--- client/pages/dashboard.tsx
+++ client/pages/dashboard.tsx
@@ -1,8 +1,27 @@
 import { SpacedSubTypography } from "../components";
 import { PageLayout } from "../layouts";
 import { DashboardTabs } from "../components/User/";
+import useAuth from "../utils/hooks/useAuth";
+import { useRouter } from "next/router";
+import { Box, CircularProgress } from "@mui/material";
 
 export default function Dashboard() {
+  const { isLoading, user } = useAuth();
+  const router = useRouter();
+
+  if (isLoading) {
+    return (
+      <Box
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
+  if (!user) {
+    router.push(`/`).catch((e) => console.log(e));
+    return <></>;
+  }
   return (
     <PageLayout
       title={"Account Page"}
